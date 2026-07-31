aaeff85bdd08b717d760cca33455fbc40d6cba28
Fateme Heydari <phtmheydari@gmail.com>
2026-07-31 13:39:20 +0330

    Add def to gradebook

--- gradebook.py
+++ gradebook.py
@@ -6,7 +6,7 @@ class Gradebook:
         self.passing_grades = {}
         self.passing_grades = 55
 
-    def add_student(self, student):
+    def add_student(self, student):        # add student
         student_id = student.get_id()
         if student_id in self.students:
             print("Student already exists.")
@@ -14,7 +14,7 @@ class Gradebook:
             self.students[student_id] = student
             print("Student added successfully.")
 
-    def add_course(self, course):
+    def add_course(self, course):          # add course
         if course.course_code in self.courses:
             print("Course already exists.")
         else:
@@ -22,8 +22,23 @@ class Gradebook:
             print("Course added successfully.")
 
 
-    def search_student(self, keyword):
+    def search_student(self, keyword):         # add student
         for student in self.students.values():
             if student.get_id() == keyword or student.get_name().lower() == keyword.lower():
                 return student
         return None
+
+
+    def enroll_student(self, student_id, course_code):      # Enroll student
+            print("Student does not found.")
+        if course_code not in self.courses:
+            return
+        student = self.students[student_id]
+        course = self.courses[course_code]
+
+    student.enroll_course(course_code)
+    course.add_student(student_id)
+        print("Student enrolled successfully.")
+
+    
+
